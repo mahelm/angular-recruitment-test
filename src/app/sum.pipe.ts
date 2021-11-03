6b3b68488f1cb94ddec48d62509d877d23973cf0
mahelm <mattias.helmer@gmail.com>
2021-11-03 11:28:56 +0100

    3 files modified

--- src/app/sum.pipe.ts
+++ src/app/sum.pipe.ts
@@ -1,10 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'sum'
+  name: 'sum',
 })
 export class SumPipe implements PipeTransform {
-
   transform(numbersSeparatedByCommas: string): any {
     const numbers = this.getNumbers(numbersSeparatedByCommas);
     const sum = this.getSum2(numbers);
@@ -14,7 +13,7 @@ export class SumPipe implements PipeTransform {
   getSum1(numbers: number[]) {
     let sum = 0;
 
-    for(let number of numbers){
+    for (let number of numbers) {
       sum += number;
     }
 
@@ -22,11 +21,13 @@ export class SumPipe implements PipeTransform {
   }
 
   getSum2(numbers: number[]) {
-      return numbers.reduce((number, sum) =>  sum + number, 0);
+    return numbers.reduce((number, sum) => sum + number, 0);
   }
 
   getNumbers(numbersSeparatedByCommas: string) {
-    return numbersSeparatedByCommas.split(',').map(i => parseInt(i.trim(), 10)).filter(i => !isNaN(i));
+    return numbersSeparatedByCommas
+      .split(',')
+      .map((i) => parseInt(i.trim(), 10))
+      .filter((i) => !isNaN(i));
   }
 }
-
