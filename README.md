9b219cae45f4ac8e497cfeac360a967d37b692b9
mahelm <mattias.helmer@gmail.com>
2021-11-03 11:14:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fjmlsc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fjmlsc)
---
+++ src/app/sum.pipe.ts
@@ -0,0 +1,32 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sum'
+})
+export class SumPipe implements PipeTransform {
+
+  transform(numbersSeparatedByCommas: string): any {
+    const numbers = this.getNumbers(numbersSeparatedByCommas);
+    const sum = this.getSum2(numbers);
+    return sum;
+  }
+
+  getSum1(numbers: number[]) {
+    let sum = 0;
+
+    for(let number of numbers){
+      sum += number;
+    }
+
+    return sum;
+  }
+
+  getSum2(numbers: number[]) {
+      return numbers.reduce((number, sum) =>  sum + number, 0);
+  }
+
+  getNumbers(numbersSeparatedByCommas: string) {
+    return numbersSeparatedByCommas.split(',').map(i => parseInt(i.trim(), 10)).filter(i => !isNaN(i));
+  }
+}
+
